0a827387ee5f6dd831f65818127950bfb3653626
necron9x11 <necron9x11@protonmail.com>
2018-10-02 10:17:37 -0500

    initial commit

---
+++ config/fileTemplates/Python Script - Violent Python Project Header.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+#
+# Filename: vulnscanner.py
+#
+# Violent Python - A Cookbook for Hackers,Forensic Analysts,Penetration Testers and Security Engineers
+# Chapter < N > | < EXAMPLE DESCRIPTION > - version < N >
+#
+# NOTE: This is a modified version of the final version of the < DESCRIPTION > from pages < N > through < N >.
+#
+#       The original version supplied with the book and downloaded from:
+#
+#       https://booksite.elsevier.com/9781597499576/?ISBN=9781597499576
+#
+#       and is saved here, for comparison, as < FILENAME >
+
+# Original Author: TJ O'Connor
+# Modified to run under Python 3.x by: Daniel Raphael
+#
